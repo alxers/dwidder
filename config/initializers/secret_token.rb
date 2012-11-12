8b59a3277ae295f1b715036e5cfc09f0ec5bc03c
alxers <alex4rom@gmail.com>
2012-11-12 19:13:10 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dwidder::Application.config.secret_token = 'c6ff1b569f92bba58c7f5b5f2d6551a197d10926d993af6ff6e8830de71d352b4333589b55876b148e8ac9a1ad6b65a04720cb9179047dc9128a698595a558a7'
